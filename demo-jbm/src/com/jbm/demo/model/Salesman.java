599d3f94516b32ea2c30423f308df8d61684ee23
johanpbm <johan.baldeon@pucp.edu.pe>
2018-11-21 02:04:35 -0500

    Commit the Salesman class.

---
+++ demo-jbm/src/com/jbm/demo/model/Salesman.java
@@ -0,0 +1,13 @@
+package com.jbm.demo.model;
+
+public class Salesman {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
